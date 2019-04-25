ee8c5961cc2515cc48820380a08cd84f268f7812
blbst123 <billloukehao@gmail.com>
2019-04-24 20:41:47 -0700

    Implement actions, reducers, store

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,6 +5,13 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
+
+  return (dispatch) => {
+    searchYouTube({key: YOUTUBE_API_KEY, query: q, max: 5}, function(data) {
+      dispatch(changeVideo(data[0]));
+      dispatch(changeVideoList(data));
+    });
+  };
  
   //TODO:  Write an asynchronous action to handle a video search!
 };
